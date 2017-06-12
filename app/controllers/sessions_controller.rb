ede6ea905caac299412298297986b561f204f92b
gamrani <gamraniyoussef@gmail.com>
2017-06-12 22:22:13 +0000

    SESSION FINAL

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
 
   def create
 
-    @user = User.find_by(email: params[:session][:email].downcase)
-     if @user && @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+     if !user.nil?
        # Log the user in and redirect to the user's show page.
-       render 'home'
+       redirect_to root_path
      else
        # Create an error message.
        flash[:danger] = 'Invalid email/password combination' # Not quite right!
