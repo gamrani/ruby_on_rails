d5f01eda1e55d06f509e17aedd07ea9daf927ca6
gamrani <gamraniyoussef@gmail.com>
2017-06-13 11:51:53 +0000

    PROBLEM REGLE

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'puma', '~> 3.7'
 gem  'bcrypt' , '3.1.11'
 gem 'jquery-rails'
 gem 'bootstrap-sass',  '3.2.0.0'
+gem 'rails-controller-testing'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
        redirect_to root_path
      else
        # Create an error message.
-       flash[:danger] = 'Invalid email/password combination' # Not quite right!
+       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
        render 'new'
      end
 
