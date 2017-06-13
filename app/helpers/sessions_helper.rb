48e4ecce086d517724ac2dc23cd3cacab33b10e7
gamrani <gamraniyoussef@gmail.com>
2017-06-13 11:38:31 +0000

    INSCRIPTION LINK FINISHED - LOGIN PROBLEM

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+  def log_in(utilisateur)
+	session[:utilisateur_id]=utilisateur.id
+end
+
+
+def utilisateur_courant
+	@utilisateur_courant=@utilisateur_courant ||
+	User.find_by(id: session[:user_id])
+end
+
+def logged_in?
+	!utilisateur_courant.nil?
+end
+
+
+def log_out
+	session.delete(:utilisateur_id)
+	@utilisateur_courant=nil
+end
 end
