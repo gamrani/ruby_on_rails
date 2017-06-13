91c5c8cb636eceb201d4de6c6c63d249c14ec2e9
gamrani <gamraniyoussef@gmail.com>
2017-06-13 15:10:18 +0000

    b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,24 @@ class UsersController < ApplicationController
   def home
   end
 
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to root_path
+
+    else
+      render 'edit'
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get    '/login',   to: 'sessions#new'
     post   '/login',   to: 'sessions#create'
     delete '/logout',  to: 'sessions#destroy'
-
-    resources :users , only:[:new,:create]
+   get '/edit', to: 'users#edit'
+    resources :users 
 end
